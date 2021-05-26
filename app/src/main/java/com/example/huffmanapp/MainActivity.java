60cf0268dffc2005b0823f7683f6698013b6113b
geon <przychodzien.lukasz98@gmail.com>
2021-05-26 13:28:28 +0200

    hotfix

--- app/src/main/java/com/example/huffmanapp/MainActivity.java
+++ app/src/main/java/com/example/huffmanapp/MainActivity.java
@@ -60,7 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onBindViewHolder(TreeViewHolder viewHolder, Object data, int position) {
-            viewHolder.nodeCode.setText(data.toString()); }
+                viewHolder.nodeCode.setText(data.toString());
+            }
         };
         huffmanTree.setAdapter(treeAdapter);
         huffmanTree.setOnTouchListener((v, event) -> {
@@ -95,35 +96,34 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void calculateHuffman(View view) {
-        nodeArrayList =new ArrayList<>();
+        nodeArrayList = new ArrayList<>();
         listView.setAdapter(null);
         entropyTextView.setText(getString(R.string.entropia));
         userText = Objects.requireNonNull(inputTextField.getText()).toString();
-        if(userText.length()>0)
-        calcCharactersList();
+        if (userText.length() > 0)
+            calcCharactersList();
         codedLetters = new HashMap<>();
     }
-    private void calcCharactersList()
-    {   node= new ArrayList<>();
-        for(int i=0;i<userText.length();i++)
-        {   boolean wasOccurred = false;
-            for (Node m: node)
-            {
-                wasOccurred = m.getLetter()==userText.charAt(i);
-                if(wasOccurred)
-                {
-                    m.setCounter(m.getCounter()+1);
+
+    private void calcCharactersList() {
+        node = new ArrayList<>();
+        for (int i = 0; i < userText.length(); i++) {
+            boolean wasOccurred = false;
+            for (Node m : node) {
+                wasOccurred = m.getLetter() == userText.charAt(i);
+                if (wasOccurred) {
+                    m.setCounter(m.getCounter() + 1);
                     break;
                 }
             }
-            if(!wasOccurred)
+            if (!wasOccurred)
                 node.add(new Node(userText.charAt(i)));
         }
         nodeArrayList = new ArrayList<>();
         calcEntropy();
         treeMaker();
         codedLetters = new HashMap<>();
-        TreeNode father = new TreeNode(node.get(0).getLetter()!=0 ? node.get(0).getLetter() +"\n1" : node.get(0).getCounter());
+        TreeNode father = new TreeNode(node.get(0).getLetter() != 0 ? node.get(0).getLetter() + "\n1" : node.get(0).getCounter());
         calcHuffman(node.get(0), "", father);
         rowAdapter = new RowAdapter(this, nodeArrayList);
         treeAdapter.setRootNode(father);
@@ -131,77 +131,74 @@ public class MainActivity extends AppCompatActivity {
         translateText();
         calcKeyWordLength();
     }
-    private void translateText()
-    {   String translatedTextt="";
-        for(int i=0;i<userText.length();i++)
-        {
+
+    private void translateText() {
+        String translatedTextt = "";
+        for (int i = 0; i < userText.length(); i++) {
             translatedTextt = translatedTextt.concat(codedLetters.get(userText.charAt(i)));
         }
         translatedTextView.setText("Tekst w kodzie Huffmana: ".concat(translatedTextt));
 
     }
-    private void calcHuffman(Node node, final String code, TreeNode fatherNode)
-    {   TreeNode left, right;
-        if(node.getNodeLeft()!=null)
-        {
 
-            String nodeText = node.getNodeLeft().getLetter()!= 0 ? node.getNodeLeft().getCounter()+"\n\""+node.getNodeLeft().getLetter()+"\"\n" + code.concat(Integer.toString(0)) : Integer.toString(node.getNodeLeft().getCounter());
-            left= new TreeNode(nodeText);
-            calcHuffman(node.getNodeLeft(),code+0, left);
+    private void calcHuffman(Node node, final String code, TreeNode fatherNode) {
+        TreeNode left, right;
+        if (node.getNodeLeft() != null) {
+
+            String nodeText = node.getNodeLeft().getLetter() != 0 ? node.getNodeLeft().getCounter() + "\n\"" + node.getNodeLeft().getLetter() + "\"\n" + code.concat(Integer.toString(0)) : Integer.toString(node.getNodeLeft().getCounter());
+            left = new TreeNode(nodeText);
+            calcHuffman(node.getNodeLeft(), code + 0, left);
             fatherNode.addChild(left);
         }
-        if(node.getNodeRight()!=null)
-        {   String nodeText = node.getNodeRight().getLetter() != '\0' ? node.getNodeRight().getCounter()+"\n\"" + node.getNodeRight().getLetter() +"\"\n"+code.concat(Integer.toString(1)) : Integer.toString(node.getNodeRight().getCounter());
+        if (node.getNodeRight() != null) {
+            String nodeText = node.getNodeRight().getLetter() != '\0' ? node.getNodeRight().getCounter() + "\n\"" + node.getNodeRight().getLetter() + "\"\n" + code.concat(Integer.toString(1)) : Integer.toString(node.getNodeRight().getCounter());
             right = new TreeNode(nodeText);
-            calcHuffman(node.getNodeRight(), code+1, right);
+            calcHuffman(node.getNodeRight(), code + 1, right);
             fatherNode.addChild(right);
-        }
-        else if(node.getNodeLeft()==null) {
-            node.setHuffmanCode(code.length()>0? code : "1");
-            codedLetters.put(node.getLetter(),code.length()>0 ? code : "1");
+        } else if (node.getNodeLeft() == null) {
+            node.setHuffmanCode(code.length() > 0 ? code : "1");
+            codedLetters.put(node.getLetter(), code.length() > 0 ? code : "1");
             nodeArrayList.add(node);
         }
     }
 
 
-
     private void treeMaker() {
         Node left, right;
-        while (node.size()>1){
+        while (node.size() > 1) {
             left = findTheSmallestInList();
             right = findTheSmallestInList();
             node.add(new Node(left, right));
-        }}
+        }
+    }
 
-    private Node findTheSmallestInList()
-    {   int index=0;
+    private Node findTheSmallestInList() {
+        int index = 0;
         Node newNode = node.get(0);
-        for (int i = 1; i< node.size(); i++) {
-            if(newNode.getCounter() > node.get(i).getCounter())
-            {
-                newNode= node.get(i);
-                index=i;
+        for (int i = 1; i < node.size(); i++) {
+            if (newNode.getCounter() > node.get(i).getCounter()) {
+                newNode = node.get(i);
+                index = i;
             }
         }
         node.remove(index);
         return newNode;
     }
 
-    private void calcEntropy()
-    {
-        entropy=0;
-        node.forEach((tmp)->{
-            double p = tmp.getCounter()/(double)userText.length();
-            entropy += p * (Math.log(1.0/p) / Math.log(2));
+    private void calcEntropy() {
+        entropy = 0;
+        node.forEach((tmp) -> {
+            double p = tmp.getCounter() / (double) userText.length();
+            entropy += p * (Math.log(1.0 / p) / Math.log(2));
         });
-                DecimalFormat df = new DecimalFormat("#.#####");
+        DecimalFormat df = new DecimalFormat("#.#####");
         entropyTextView.setText(entropyTextView.getText().toString().concat("\n").concat(df.format(entropy)));
     }
-    private void calcKeyWordLength()
-    {
-        keyWordLength=0;
-        nodeArrayList.forEach((tmp)->{
-            double p = tmp.getCounter()/(double)userText.length();
+
+    private void calcKeyWordLength() {
+        keyWordLength = 0;
+        nodeArrayList.forEach((tmp) -> {
+            double p = tmp.getCounter() / (double) userText.length();
             keyWordLength += p * tmp.getHuffmanCode().length();
         });
         DecimalFormat df = new DecimalFormat("#.#####");
@@ -211,5 +208,9 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     public void sheSellsGenerateText(View view) {
         inputTextField.setText("She sells seashells by the seashore, The shells she sells are seashells, I'm sure. So if she sells seashells on the seashore, Then I'm sure she sells seashore shells.");
+    }
+
+    public void whoAmIgenerateText(View view) {
+        inputTextField.setText("Łukasz Przychodzień Teleinformatyka");
     }
 }
